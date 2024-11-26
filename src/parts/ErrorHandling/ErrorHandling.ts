537eec1df4c2b5b60f45ed23f442bb63f57f0e3a
Le Vivilet <levivlet@pm.me>
2024-11-26 21:30:59 +0100

    feature: remove unused code

--- src/parts/ErrorHandling/ErrorHandling.ts
+++ src/parts/ErrorHandling/ErrorHandling.ts
@@ -6,47 +6,6 @@ import * as Logger from '../Logger/Logger.ts'
 import * as PrettyError from '../PrettyError/PrettyError.ts'
 import * as Process from '../Process/Process.ts'
 
-export const state = {
-  seenErrors: [],
-}
-
-const preparePrettyError = (error) => {
-  const cause = error.cause()
-  return {
-    message: error.message,
-    codeFrame: cause.codeFrame,
-    stack: cause.stack,
-  }
-}
-
-const printPrettyError = (prettyError) => {
-  console.error(
-    `${prettyError.message}\n${prettyError.codeFrame}\n${prettyError.stack}`,
-  )
-}
-
-export const handleError = (error) => {
-  // @ts-ignore
-  if (state.seenErrors.includes(error.message)) {
-    return
-  }
-  // @ts-ignore
-  state.seenErrors.push(error.message)
-  const prettyError = preparePrettyError(error)
-  // Socket.send({
-  //   jsonrpc: JsonRpcVersion.Two,
-  //   method: /* Dialog.showErrorDialogWithOptions */ 'Dialog.showMessage',
-  //   params: [
-  //     {
-  //       message: prettyError.message,
-  //       codeFrame: prettyError.codeFrame,
-  //       stack: prettyError.stack,
-  //     },
-  //   ],
-  // })
-  printPrettyError(prettyError)
-}
-
 const firstErrorLine = (error) => {
   if (error.stack) {
     return error.stack.slice(0, GetNewLineIndex.getNewLineIndex(error.stack))
@@ -58,7 +17,7 @@ const firstErrorLine = (error) => {
 }
 
 // @ts-ignore
-export const handleUncaughtExceptionMonitor = (error, origin) => {
+export const handleUncaughtExceptionMonitor = (error) => {
   Logger.info(`[pty host] uncaught exception: ${firstErrorLine(error)}`)
   if (IsIgnoredError.isIgnoredError(error)) {
     return
