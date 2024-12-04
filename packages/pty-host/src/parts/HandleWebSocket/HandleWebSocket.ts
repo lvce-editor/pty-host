d468219adb4f09d3875dd711944bef93a4eb02fe
Le Vivilet <levivlet@pm.me>
2024-12-04 19:53:01 +0100

    feature: simplify rpc

--- packages/pty-host/src/parts/HandleWebSocket/HandleWebSocket.ts
+++ packages/pty-host/src/parts/HandleWebSocket/HandleWebSocket.ts
@@ -1,15 +1,13 @@
 import * as Assert from '../Assert/Assert.ts'
-import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 import * as IpcChild from '../IpcChild/IpcChild.ts'
 import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
 
 export const handleWebSocket = async (handle, request) => {
   Assert.object(handle)
   Assert.object(request)
-  const ipc = await IpcChild.listen({
+  await IpcChild.listen({
     method: IpcChildType.WebSocket,
     request,
     handle,
   })
-  HandleIpc.handleIpc(ipc)
 }
