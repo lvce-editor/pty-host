b5b91669e92e2531b41d345c9b3d400f84186127
Le Vivilet <levivlet@pm.me>
2025-06-12 00:10:29 +0200

    fix: missing rpc option

--- packages/pty-host/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.ts
+++ packages/pty-host/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.ts
@@ -1,10 +1,12 @@
 import { ElectronMessagePortRpcClient } from '@lvce-editor/rpc'
 import * as Assert from '../Assert/Assert.ts'
+import * as RequiresSocket from '../RequiresSocket/RequiresSocket.ts'
 
 export const handleElectronMessagePort = async (messagePort) => {
   Assert.object(messagePort)
   await ElectronMessagePortRpcClient.create({
     commandMap: {},
     messagePort,
+    requiresSocket: RequiresSocket.requiresSocket,
   })
 }
