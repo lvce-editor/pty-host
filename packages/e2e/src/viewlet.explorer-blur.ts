d998dfb3b73c501f92ab37752fc511381d1d83b0
Le Vivilet <levivilet@pm.me>
2026-07-12 17:16:48 +0000

    fix: resolve eslint workflow and e2e errors

--- packages/e2e/src/viewlet.explorer-blur.ts
+++ packages/e2e/src/viewlet.explorer-blur.ts
@@ -11,9 +11,11 @@ export const test: Test = async ({
 }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
-  await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'content 1')
-  await FileSystem.writeFile(`${tmpDir}/file2.txt`, 'content 2')
-  await FileSystem.writeFile(`${tmpDir}/file3.txt`, 'content 3')
+  await FileSystem.setFiles([
+    { content: 'content 1', uri: `${tmpDir}/file1.txt` },
+    { content: 'content 2', uri: `${tmpDir}/file2.txt` },
+    { content: 'content 3', uri: `${tmpDir}/file3.txt` },
+  ])
   await Workspace.setPath(tmpDir)
   await Explorer.selectIndices([0, 1])
 
